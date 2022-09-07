a11162f32f548542e6dd6c5370a6bb3a278a15a7
wuchengqian <h78c132@163.com>
2022-09-07 21:10:10 +0800

    第二次

--- git/src/main/java/Test.java
+++ git/src/main/java/Test.java
@@ -6,6 +6,7 @@ public class Test {
     //这是一个main方法,是程序的入口:
     public static void main(String[] args) {
         System.out.println("git_01");
+        System.out.println("git_02");
     }
 }
     
