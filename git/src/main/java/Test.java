afc56bae84718399bdc49d6a4c000c3a484861ef
wuchengqian <h78c132@163.com>
2022-09-07 21:09:42 +0800

    第一次

---
+++ git/src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author: 吴成前
+ * @create: 2022-09-07 21:08
+ */
+public class Test {
+    //这是一个main方法,是程序的入口:
+    public static void main(String[] args) {
+        System.out.println("git_01");
+    }
+}
+    
+    
+    
